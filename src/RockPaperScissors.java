7166c5adc210ee9f03434ae0ea4f598dd29d9774
irenechau31 <beasthoang01@gmail.com>
2023-06-18 22:05:27 +0700

    ADDED VALID INPUT

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -9,33 +9,43 @@ public class RockPaperScissors {
         while (true){
             System.out.println("Do you want to play again Y or N");
             playAgain = in.nextLine();
-            if (Objects.equals(playAgain, "Y")) {break;}
-                System.out.println("Enter R for rock, P for paper, S for scissor.");
+            if (!Objects.equals(playAgain, "Y")) {break;}
+            System.out.println("Enter R for rock, P for paper, S for scissor.");
+            while (true){
                 System.out.println("Player A, enter your move (R/P/S): ");
                 moveA = in.nextLine();
+                //Valid A input
+                if((moveA.equalsIgnoreCase("R")||moveA.equalsIgnoreCase("P")||moveA.equalsIgnoreCase("S"))){break;}
+                {System.out.println("Invalid move, please enter R for rock, P for paper, S for scissor. ");}
+            }
+            while (true){
                 System.out.println("Player B, enter your move (R/P/S): ");
                 moveB = in.nextLine();
-                if (moveA.equalsIgnoreCase("R")){
-                    if (moveB.equalsIgnoreCase("R")) {
+                //Valid B input
+                if((moveB.equalsIgnoreCase("R")||moveB.equalsIgnoreCase("P")||moveB.equalsIgnoreCase("S"))){break;}
+                {System.out.println("Invalid move, please enter R for rock, P for paper, S for scissor. ");}
+            }
+            if (moveA.equalsIgnoreCase("R")){
+                if (moveB.equalsIgnoreCase("R")) {
                         System.out.println("it's a tie");}
-                    else if (moveB.equalsIgnoreCase("P")) {
+                else if (moveB.equalsIgnoreCase("P")) {
                         System.out.println("Paper covers Rock, player A loses!");}
-                    else {System.out.println("Rock break Scissors, player A wins!");}
-                }
-                else if (moveA.equalsIgnoreCase("P")){
-                    if (moveB.equalsIgnoreCase("R")) {
+                else {System.out.println("Rock break Scissors, player A wins!");}
+            }
+            else if (moveA.equalsIgnoreCase("P")){
+                if (moveB.equalsIgnoreCase("R")) {
                         System.out.println("Paper covers Rock, player A wins");}
-                    else if (moveB.equalsIgnoreCase("P")) {
+                else if (moveB.equalsIgnoreCase("P")) {
                         System.out.println("It's a tie");}
-                    else {System.out.println("Scissors cuts Paper, player A loses!");}
-                }
-                else {
-                    if (moveB.equalsIgnoreCase("R")) {
+                else {System.out.println("Scissors cuts Paper, player A loses!");}
+            }
+            else {
+                if (moveB.equalsIgnoreCase("R")) {
                         System.out.println("Rock breaks Scissors, player A wins");}
-                    else if (moveB.equalsIgnoreCase("P")) {
+                else if (moveB.equalsIgnoreCase("P")) {
                         System.out.println("Scissors cuts Paper, player A wins!");}
-                    else {System.out.println("It's a tie");}
-                }
+                else {System.out.println("It's a tie");}
             }
         }
     }
+}
